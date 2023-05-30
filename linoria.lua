0eb89299faf10b8ca8c9bc11faeb233c98825cac
Skeereddo <124922705+Skeereddo@users.noreply.github.com>
2023-05-31 00:32:50 +0200

    Update linoria.lua

--- linoria.lua
+++ linoria.lua
@@ -2519,7 +2519,18 @@ function Library:CreateWindow(WindowTitle)
             Cursor.Thickness = 1;
             Cursor.Filled = true;
 
-            
+            while Outer.Visible do
+                local mPos = workspace.CurrentCamera:WorldToViewportPoint(Mouse.Hit.p);
+
+                Cursor.Color = Library.AccentColor;
+                Cursor.PointA = Vector2.new(mPos.X, mPos.Y);
+                Cursor.PointB = Vector2.new(mPos.X, mPos.Y) + Vector2.new(6, 14);
+                Cursor.PointC = Vector2.new(mPos.X, mPos.Y) + Vector2.new(-6, 14);
+
+                Cursor.Visible = not InputService.MouseIconEnabled;
+
+                RenderStepped:Wait();
+            end;
 
             Cursor:Remove();
         end;
